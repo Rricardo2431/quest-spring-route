7ced6940bc2beffe2f5907e4da9e72b51f8ce091
Rricardo2431 <128594768+Rricardo2431@users.noreply.github.com>
2023-07-18 10:57:13 +0200

    quete 2 Spring route

--- src/main/java/com/wildcodeschool/doctor/DoctorWhoApplication.java
+++ src/main/java/com/wildcodeschool/doctor/DoctorWhoApplication.java
@@ -8,6 +8,7 @@ public class DoctorWhoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorWhoApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,29 @@
 package com.wildcodeschool.doctor.controller;
 
-import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.wildcodeschool.doctor.model.Doctor;
 
 @Controller
 public class DoctorController {
-
-    @GetMapping("/doctor/")
+    /*@GetMapping("/")
+    public String index() {
+        return "index.html";
+    }*/
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable int number) {
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number > 0 && number < 13) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "303 See Other");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
     }
 }
